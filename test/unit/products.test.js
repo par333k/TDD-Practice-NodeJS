0df947695ade336c300ec3a2a219c2456bb9a93f
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-03-31 17:45:41 +0900

    feat: Write sample test code using jest

---
+++ test/unit/products.test.js
@@ -0,0 +1,11 @@
+const { describe, test, expect } = require("@jest/globals");
+
+describe('Calculation', () => {
+    test('two plus two is four', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+    test('two plus two is not five', () => {
+        expect(2 + 2).not.toBe(5);
+    });
+});
