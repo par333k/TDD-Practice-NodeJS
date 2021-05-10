455a514c91afff117d22c5aec77c8c481900f4f8
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-10 23:23:20 +0900

    test: Write unit test code for CREATE Error handling

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -20,7 +20,8 @@ let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
-    next = null;
+    // 비동기 요청에 대한 에러 응답을 처리하기 위해 next도 mock 함수로 작성해준다.
+    next = jest.fn();
 });
 
 describe('Product Controller Create', () => {
@@ -32,28 +33,44 @@ describe('Product Controller Create', () => {
     test('should have a createProduct function', () => {
         expect(typeof productController.createProduct).toBe('function');
     });
-    test('Should call ProductModel.create', () => {
+    test('Should call ProductModel.create', async() => {
         // createProduct() 함수가 호출이 될때,
         // 위에서 httpMocks로 생성해준 req, res, next를 인수로 넘겨서
         // createProduct함수를 호출해준다. 
-        productController.createProduct(req, res, next);
+        await productController.createProduct(req, res, next);
         // productModel의 create 메소드가 같이 호출되는지 확인
         expect(productModel.create).toBeCalledWith(newProduct);
     });
-    test('should return 201 response code', () => {
-        productController.createProduct(req, res, next);
+    test('should return 201 response code', async() => {
+        await productController.createProduct(req, res, next);
         expect(res.statusCode).toBe(201);
         expect(res._isEndCalled()).toBeTruthy();
     });
-    test('should return json body in response', () => {
+    test('should return json body in response', async() => {
         // mock 함수로 선언한 create 함수의 반환값을 mockReturnValue를 통해
         // 새로 추가한 product의 json 데이터를 반환하도록 지정한다.
         productModel.create.mockReturnValue(newProduct);
         // productController의 createProduct function의 인수로 
         // mock request, response, next의 값을 넣어서 호출한다.
-        productController.createProduct(req, res, next);
+        await productController.createProduct(req, res, next);
         // 호출한 결과, response 객체에 json data가 지정한 json 데이터 반환값과
         // 일치하는지 확인을 한다.
         expect(res._getJSONData()).toStrictEqual(newProduct);
+    });
+
+    /*
+    mongo db에서 처리하는 부분은 문제가 없다는 것을 가정하는 단위테스트이기 때문에
+    원래 mongo db에서 처리하는 에러 메시지 부분은 Mock 함수를 이용해서 처리한다.
+    (잘 처리되고 있다고 가정)
+    */
+    test('should handle errors', async() => {
+        const errorMessage = { message: 'description property missing' };
+        // 인위적으로 에러메시지를 만들어서 전달한다.
+        // Promise 객체에 담아서 전달해준다.
+        // Promise.reject에 넣어주는 이유는 요청을 받
+        const rejectedPromise = Promise.reject(errorMessage);
+        productModel.create.mockReturnValue(rejectedPromise);
+        await productController.createProduct(req, res, next);
+        expect(next).toBeCalledWith(errorMessage);
     });
 });
