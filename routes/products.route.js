a57aed376df94c2b5c063f6a2d390488eeaff892
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-12 00:06:59 +0900

    feat: Add new router for put request method (route to updateProduct callback function)

--- routes/products.route.js
+++ routes/products.route.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/products.controller');
 router.get('/', productController.getProducts);
 router.post('/', productController.createProduct);
 router.get('/:productId', productController.getProductById);
+router.put('/:productId', productController.updateProduct);
 
 module.exports = router;
