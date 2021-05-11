1cd39117a2054f65ed7b9d8def01731a4f695353
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-11 14:30:49 +0900

    test: Write unit test code for testing READ
    - Add mock data to test getProducts callback function
    - Remove console.log function from existing integration testing code

--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -18,6 +18,5 @@ test('should return 500 on POST /api/products', async() => {
         // 에러를 인위적으로 발생시키기 위해 아래와 같은 데이터 전달
         .send({ name: "phone" });
     expect(response.statusCode).toBe(500);
-    console.log('response.body', response.body)
     expect(response.body).toStrictEqual({ message: "Product validation failed: description: Path `description` is required." })
 });
